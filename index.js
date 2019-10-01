8970d4e24b77dfc061d4fb7dfee981f1f16f6e22
trd-cliff <54255826+trd-cliff@users.noreply.github.com>
2019-10-01 17:17:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import circle from './Vector.png';
+import sky from './bright-sky.png';
+import background from './backgroundImage.jpeg';
+import card from './cardImage.jpeg';
+import logourl from './LTS.png';
+import Vector_downArrow from './Vector_downArrow.png';
+import seatdesign1 from './design1.svg';
+import seatdesign2 from './designType2.svg';
+import seatdesign3 from './designType3.svg';
+import headerImg from './bright-sky.png';
+
+export {
+    circle,
+    sky,
+    card,
+    background,
+    logourl,
+    Vector_downArrow,
+    seatdesign1,
+    seatdesign2,
+    seatdesign3,
+    headerImg
+}
